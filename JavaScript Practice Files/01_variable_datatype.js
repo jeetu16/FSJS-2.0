c8ad5f7f198e7e9aa96c5681d0d1b37622374e9a
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-04 21:06:15 +0530

    variable and datatype in js

---
+++ JavaScript Practice Files/01_variable_datatype.js
@@ -0,0 +1,20 @@
+var fullName = "Jeetu Dewangan";
+
+var courseName = "JavaScript Bootcamp";
+
+const stdId = 101;
+// stdId = 4;         // Error because of constant variable
+
+let loggedCount = 34;
+
+var paymentMode;
+
+console.log(paymentMode);
+paymentMode = "Credit Card";
+console.log(paymentMode);
+
+console.log(`
+Student ID       : ${stdId}
+Full Name        : ${fullName}
+Course Name      : ${courseName}
+`)
