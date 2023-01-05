b64f64fbe25a362389a95b151be110d7bd9fb404
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-05 20:50:28 +0530

    Conditional Statement file added

---
+++ JavaScript Practice Files/02_conditional_statement.js
@@ -0,0 +1,34 @@
+var email = false;
+var facebook = false;
+var google = false;
+
+// if-else conditional statement
+if (email || facebook || google){
+    console.log("Login Success");
+}
+else{
+    console.log("Login Failed");
+}
+
+
+
+var marks = 44;
+//  if-else-if conditional statement
+if(marks >= 90 ){
+    console.log("Honors");
+}
+else if(marks >= 75){
+    console.log("Grade A");
+}
+else if(marks >= 60){
+    console.log("Grade B");
+}
+else if(marks >= 45) {
+    console.log("Grade C");
+}
+else if(marks >= 35){
+    console.log("Grade D");
+}
+else{
+    console.log("You are fail");
+}
