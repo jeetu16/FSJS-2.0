4e70d5342ab9c39e2b0da14779e8a439b67b142e
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-06 11:49:55 +0530

    Loops file added

---
+++ JavaScript Practice Files/04_loops.js
@@ -0,0 +1,26 @@
+
+// do while loop : executated atleast one time
+let i = 6;
+do {
+    if (i <= 5) {
+        console.log("Student " + i + " got a chocolate");
+    } else {
+        console.log("You are lucky one");
+    }
+    i++;
+} while (i <= 5);
+
+
+
+// while loop
+i = 1;
+while (i <= 5) {
+    console.log("Student " + i + " got a chocolate");
+    i++;
+}
+
+// for loop
+let j = 5;
+for (let i = 1; i <= 10; i++) {
+    console.log(`${j} * ${i} = ${j*i}`);
+}
