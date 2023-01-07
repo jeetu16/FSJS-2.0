cad3d15b0104b846f14659f20363822718308ba4
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-07 12:31:56 +0530

    array and object files added

---
+++ JavaScript Practice Files/05_arrays.js
@@ -0,0 +1,6 @@
+let students = ['Ajay','Jeetu','Ram','Shyam', 'Ramesh', 'Suresh', 'Bishal']; //string array
+
+let user = [1,'Kamal',true];   // mix array 
+console.log(students);
+console.log(students[1]);
+console.log(user);
---
+++ JavaScript Practice Files/06_object.js
@@ -0,0 +1,12 @@
+let bankDetails = {
+    firstName: 'Jeetu',
+    lastName: 'Dewangan',
+    accoundNumber: 22113344556677,
+    mobileNo:1234567890,
+    balance: 10000
+}
+
+console.log(bankDetails);
+
+console.log(bankDetails.accoundNumber);
+console.log(bankDetails['firstName']);
