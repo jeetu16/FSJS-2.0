eecaa1a03aa308fc93b9c359c1e8bb2e967f0357
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-08 23:48:27 +0530

    math object added

---
+++ JavaScript Practice Files/07_math_object.js
@@ -0,0 +1,21 @@
+const PI = Math.PI;
+
+console.log(PI);
+
+console.log("Round of 9.9 is " + Math.round(9.9));
+console.log("Round of 9.4 is " + Math.round(9.4));
+
+console.log("Floor of 9.9 is " + Math.floor(9.9));
+console.log("Ceil of 9.1 is " + Math.ceil(9.1));
+
+console.log("Min Value amongs (10,1,2,5,8,3): " + Math.min(10,1,2,5,8,3));
+console.log("Max Value amongs (10,1,2,5,8,3): " + Math.max(10, 1, 2, 5, 8, 3));
+
+console.log("Random Number between 0 to 1: " + Math.random());
+console.log("Random Number between 0 to 15: " + Math.round(Math.random()*15));
+
+console.log("3 to the power 3 is: " + Math.pow(3,3));
+
+console.log("Log of 2 is :" + Math.log(2));
+
+console.log("Sin(90) : " + Math.sin(45));
