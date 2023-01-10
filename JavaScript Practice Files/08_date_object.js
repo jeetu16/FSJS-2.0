8bc943f3c7645f44cc20015c116c22cd169d88ea
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-10 22:10:20 +0530

    Date Object file added

---
+++ JavaScript Practice Files/08_date_object.js
@@ -0,0 +1,20 @@
+const now = new Date();
+
+console.log(now);
+
+console.log(now.toString());
+
+console.log(now.getFullYear());
+
+console.log(now.getMonth());
+
+console.log(now.getDay());
+
+console.log(now.getTime());
+
+console.log(now.getHours());
+
+console.log(now.getMinutes());
+
+console.log(now.getSeconds());
+
