6924823bd1e86c274d77d3864d7390d34dd26b67
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-11 11:49:07 +0530

    Q2 Solved

---
+++ JavaScript Assignment/02/personal_info.js
@@ -0,0 +1,13 @@
+let firstName = "Jeetu";
+let lastName = "Dewangan";
+let maritalStatus = "Single";
+let country = "India";
+let age = 25;
+
+console.log(`
+First Name      : ${firstName}
+Last Name       : ${lastName}
+Marital Status  : ${maritalStatus}
+Country         : ${country}
+Age             : ${age}
+`)
