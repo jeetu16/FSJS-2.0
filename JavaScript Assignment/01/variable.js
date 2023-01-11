1acacba8eea1618e89a77f4da5fe90054abc7f65
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-11 11:42:08 +0530

    Q1 solved

---
+++ JavaScript Assignment/01/variable.js
@@ -0,0 +1,12 @@
+let fullName = "Jeetu Dewangan";
+let loggedIn = true;
+let jobStatus = undefined;
+let postOnMedia = null;
+let numberofConnection = 500;
+
+
+console.log('Full Name:'+fullName+", "+"Data Type: ", typeof fullName);
+console.log('Login Status:'+loggedIn+", "+"Data Type: ", typeof loggedIn);
+console.log('Job Status:'+jobStatus+", "+"Data Type: ", typeof jobStatus);
+console.log('Post on Media:'+postOnMedia+", "+"Data Type: ", typeof postOnMedia);
+console.log('Number of Connection:'+numberofConnection+", "+"Data Type: ", typeof numberofConnection);
