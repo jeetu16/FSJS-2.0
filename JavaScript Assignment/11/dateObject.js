e996f8eafea2468e768a8d873ad55eb1e28de3a4
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-12 15:00:53 +0530

    Q11 solved

---
+++ JavaScript Assignment/11/dateObject.js
@@ -0,0 +1,18 @@
+let date = new Date();
+
+let currentYear = date.getFullYear();
+let currentMonth = date.getMonth()+1;
+let currentDate = date.getDate();
+let currentDay = date.getDay();
+let currentHour = date.getHours();
+let currentMinute = date.getMinutes();
+
+
+console.log("Current Year:",currentYear);
+console.log("Current Month:",currentMonth);
+console.log("Current Date:",currentDate);
+console.log("Current Day:",currentDay);
+console.log("Current Hour:",currentHour);
+console.log("Current Minunte:",currentMinute);
+
+// console.log(date);
