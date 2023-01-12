bf798b7b3654de8cd1586499373f3ae5ad789b44
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-12 11:57:33 +0530

    Q8 solved

---
+++ JavaScript Assignment/08/trimString.js
@@ -0,0 +1,7 @@
+let string = "   Jeetu Dewangan    ";
+
+let trimmedString = string.trim();
+
+
+console.log("Before Removing White Spaces from String:",string);
+console.log("After Removing White Spaces from String:", trimmedString);
