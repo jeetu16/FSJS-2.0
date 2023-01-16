a204d0ade226d2b4e8557c7524e2e940eff0666b
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-16 17:33:27 +0530

    Q26 solved

---
+++ JavaScript Assignment/26/numTable.js
@@ -0,0 +1,16 @@
+/*
+26. Write a program to print the table of any number given by the user. The format of the output should be smiliar to
+the below example-
+If the number given by the user is 2 then the output should look like this-
+    2 * 1 = 2
+    2 * 2 = 4
+    2 * 3 = 6 and so on till 2 * 10 = 20.
+*/
+
+// Answer: 
+
+let num = prompt("Enter any number");
+
+for (let i = 0; i < 10; i++) {
+    document.write(`${num} * ${i + 1} = ${num * (i + 1)}<br>`);
+}
