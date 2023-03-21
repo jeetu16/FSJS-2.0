0620175816a4b38ee0cdecc9f002b65af82dbb00
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-03-21 23:01:57 +0530

    Content Component updated

--- React Assignment/react-todo-app/src/Components/Content.js
+++ React Assignment/react-todo-app/src/Components/Content.js
@@ -1,32 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TodoList from './TodoList';
 
 
-export default function Content({
-  items,
-  handleClick,
-  handleDelete,
-  handleEdit,
-  updateItem,
-  setUpdateItem,
-  handleInputEdit,
-  focusRef
-}) {
+export default function Content({items}) {
 
   return (
     <>
-      {items.length ? (
-        <TodoList
-          items={items}
-          handleClick={handleClick}
-          handleDelete={handleDelete}
-          handleEdit={handleEdit}
-          updateItem={updateItem}
-          setUpdateItem={setUpdateItem}
-          handleInputEdit={handleInputEdit}
-          focusRef={focusRef}
-        />
-      ) : (<p style={{ marginTop: "2rem", fontSize: "1.5rem" }}>Your Todo List is empty</p>)
+      {items.length ? <TodoList items={items}/> : (<p style={{ marginTop: "2rem", fontSize: "1.5rem" }}>Your Todo List is empty</p>)
       }
     </>
   )
