eeac883a07c1b52ac5578aeceac344476f4631c3
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-03-21 22:59:58 +0530

    AddTodo Component updated

--- React Assignment/react-todo-app/src/Components/AddTodo.js
+++ React Assignment/react-todo-app/src/Components/AddTodo.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useRef, useContext } from 'react';
 import { FaPlus } from 'react-icons/fa';
+import { MyContext } from '../App';
 
-const AddTodo = ({ handleSubmit, newItem, setNewItem, focusRefAdd }) => {
+const AddTodo = () => {
+
+    const { items, setItems} = useContext(MyContext);
+    const [newItem, setNewItem] = useState('');
+    const focusRefAdd = useRef(null);
+
+    const addItem = async (title) => {
+        const id = items.length ? items[items.length - 1].id + 1 : 1;
+        const newTodo = { id, checked: false, title };
+        const listItemes = [...items, newTodo];
+        setItems(listItemes);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        focusRefAdd.current.focus();
+        const arrStr = newItem.split("");
+        const flag = arrStr.every((arr) => arr === " ");
+        if (flag) return;
+        addItem(newItem);
+        setNewItem('');
+    }
+
+
     return (
         <form className='add-form' onSubmit={handleSubmit}>
             <input
@@ -11,7 +35,7 @@ const AddTodo = ({ handleSubmit, newItem, setNewItem, focusRefAdd }) => {
                 placeholder='Add Todo'
                 required
                 value={newItem}
-                ref = { focusRefAdd }
+                ref={focusRefAdd}
                 onChange={(e) => setNewItem(e.target.value)}
 
             />
