1bb34f2e42d6632ed807021650518fb7d3df46e7
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-03-21 23:05:27 +0530

    SearchTodo Component updated

--- React Assignment/react-todo-app/src/Components/SearchTodo.js
+++ React Assignment/react-todo-app/src/Components/SearchTodo.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { MyContext } from '../App';
 
-const SearchTodo = ({ searchItem, setSearchItem }) => {
+const SearchTodo = () => {
+
+    const {searchItem, setSearchItem} = useContext(MyContext);
+
     return (
         <form className='add-form' onSubmit={(e) => e.preventDefault()}>
             <label htmlFor="search-box">Search</label>
