7426a05e531ce8df00a5b93c1961e2cc46498a7b
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-03-22 23:36:21 +0530

    redux used

--- React Assignment/react-todo-app/src/App.js
+++ React Assignment/react-todo-app/src/App.js
@@ -4,36 +4,32 @@ import Header from "./Components/Header";
 import { useState, useEffect, createContext } from "react";
 import AddTodo from "./Components/AddTodo";
 import SearchTodo from "./Components/SearchTodo";
-
-const MyContext = createContext();
+import { useSelector } from "react-redux";
 
 function App() {
 
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('myTodoList')) || []);
-  const [searchItem, setSearchItem] = useState('');
-  const [updateItem, setUpdateItem] = useState(-1);
+  const myList = useSelector((state) => state.todosList.lists);
+  const searchItem = useSelector((state) => state.todosList.searchItem);
 
   useEffect(() => {
-    localStorage.setItem("myTodoList", JSON.stringify(items));
-  }, [items])
+    localStorage.setItem("myTodoList", JSON.stringify(myList));
+  }, [myList])
 
-  
+
+
   return (
     <div className="App">
       <Header />
-      <MyContext.Provider value={{ items, setItems, searchItem, setSearchItem, updateItem, setUpdateItem }}>
-        <AddTodo />
-        { items.length ? <SearchTodo /> : null }
-        <main>
-          <Content
-            items={items.filter((item) => item.title.toLowerCase().includes(searchItem.toLowerCase()))}
-          />
-        </main>
-        <Footer />
-      </MyContext.Provider>
+      <AddTodo />
+      {myList.length ? <SearchTodo /> : null}
+      <main>
+        <Content
+          items={myList.filter((item) => item.title.toLowerCase().includes(searchItem.toLowerCase()))}
+        />
+      </main>
+      <Footer />
     </div >
   );
 }
 
-export { MyContext }
 export default App;
