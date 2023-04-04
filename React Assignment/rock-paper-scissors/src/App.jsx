3aa9eb2bf0c1dd2d496e9f7343d2f66a3f135b66
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-04-04 23:36:50 +0530

    some css added

--- React Assignment/rock-paper-scissors/src/App.jsx
+++ React Assignment/rock-paper-scissors/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   const [computerChoice, setComputerChoice] = useState("");
   const [result, setResult] = useState(false);
   const [winner, setWinner] = useState(-1);
+  const [border, setBorder] = useState(false);
 
   const handleCheck = (e) => {
     const userChoice = e.target.alt;
@@ -19,41 +20,59 @@ function App() {
     setComputerChoice(computerChoice);
     setUserChoice(userChoice);
 
-    if (userChoice === computerChoice) {
-      setWinner(-1)
-    } else if (userChoice === 'rock' && computerChoice === 'scissors' ||
-      userChoice === 'paper' && computerChoice === 'rock' ||
-      userChoice === 'scissors' && computerChoice === 'paper') {
-      setUserScore(userScore + 1);
-      setWinner(1)
-    } else {
-      setCompScore(compScore + 1);
-      setWinner(0);
+    const checkWinner = () => {
+      if (userChoice === computerChoice) {
+        setWinner(-1)
+      } else if (userChoice === 'rock' && computerChoice === 'scissors' ||
+        userChoice === 'paper' && computerChoice === 'rock' ||
+        userChoice === 'scissors' && computerChoice === 'paper') {
+        setUserScore(userScore + 1);
+        setWinner(1)
+      } else {
+        setCompScore(compScore + 1);
+        setWinner(0);
+      }
+      setBorder(true);
     }
     setResult(true);
-    setTimeout(() => setResult(false), 3000);
+
+    setTimeout(() => checkWinner(), 4000);
+  }
+
+  const handleClose = () => {
+    setBorder(false);
+    setResult(false);
   }
   return (
     <>
-      <div className="bg-slate-900 w-screen h-screen">
-        <h1 className="text-center p-4 text-3xl text-[whitesmoke] font-bold mb-8"><span className="font-mono">R</span>ock <span className="font-mono">P</span>aper <span className="font-mono">S</span>cissors</h1>
-        <div className="flex flex-col w-full h-[80vh] justify-around items-center pb-10 gap-8">
+      <div className="bg-[#020216] w-screen h-screen pt-4">
+        <h1 className="text-center p-4 text-4xl text-[whitesmoke] font-bold mb-8"><span className="font-mono">R</span>ock <span className="font-mono">P</span>aper <span className="font-mono">S</span>cissors</h1>
+        <div className="flex flex-col w-full h-[70vh] justify-around items-center pb-10 gap-8">
           <div className="flex w-full sm:justify-center">
             <div className="flex flex-col items-center w-full gap-4 sm:w-1/4 ">
-              <h2 className="font-bold text-3xl text-yellow-600 font-serif">You</h2>
-              <p className="font-semibold text-2xl text-[whitesmoke]">{userScore}</p>
+              <h2 className="font-bold text-4xl text-yellow-600 font-serif">You</h2>
+              <p className="font-semibold text-3xl text-[whitesmoke]">{userScore}</p>
             </div>
             <div className="flex flex-col items-center w-full gap-4 sm:w-1/4 ">
-              <h2 className="font-bold text-3xl text-yellow-600 font-serif">Com</h2>
-              <p className="font-semibold text-2xl text-[whitesmoke]">{compScore}</p>
+              <h2 className="font-bold text-4xl text-yellow-600 font-serif">Com</h2>
+              <p className="font-semibold text-3xl text-[whitesmoke]">{compScore}</p>
             </div>
           </div>
           <div className="flex flex-col w-full items-center gap-8">
             <h2 className="text-2xl font-semibold text-[whitesmoke] sm:text-3xl mb-3">Select Your Choice</h2>
-            <div className="flex gap-8">
-              <img onClick={(e) => handleCheck(e)} className="transform transition-all hover:scale-110 hover:cursor-pointer w-20 h-20 sm:w-28 sm:h-28" src={rock} alt="rock" />
-              <img onClick={(e) => handleCheck(e)} className="transform transition-all hover:scale-110 hover:cursor-pointer w-20 h-20 sm:w-28 sm:h-28" src={paper} alt="paper" />
-              <img onClick={(e) => handleCheck(e)} className="transform transition-all hover:scale-110 hover:cursor-pointer w-20 h-20 sm:w-28 sm:h-28" src={scissors} alt="scissors" />
+            <div className="flex gap-10 sm:gap-20">
+              <div className="flex flex-col items-center gap-4">
+                <img onClick={(e) => handleCheck(e)} className="transform transition-all hover:scale-110 hover:cursor-pointer w-20 h-20 sm:w-28 sm:h-28" src={rock} alt="rock" />
+                <p className="text-white text-2xl sm:text-3xl">Rock</p>
+              </div>
+              <div className="flex flex-col items-center gap-4">
+                <img onClick={(e) => handleCheck(e)} className="transform transition-all hover:scale-110 hover:cursor-pointer w-20 h-20 sm:w-28 sm:h-28" src={paper} alt="paper" />
+                <p className="text-white text-2xl sm:text-3xl">Paper</p>
+              </div>
+              <div className="flex flex-col items-center gap-4">
+                <img onClick={(e) => handleCheck(e)} className="transform transition-all hover:scale-110 hover:cursor-pointer w-20 h-20 sm:w-28 sm:h-28" src={scissors} alt="scissors" />
+                <p className="text-white text-2xl sm:text-3xl">Scissors</p>
+              </div>
             </div>
           </div>
         </div>
@@ -61,36 +80,65 @@ function App() {
       {
         result
         &&
-        <div className="absolute top-0 w-screen h-screen z-10 grid place-content-center text-white bg-black text-center animate-inc">
+        <div className="absolute top-0 w-screen h-screen z-10 flex flex-col items-center justify-center gap-16 text-white bg-[#04011a] text-center animate-inc">
           {
-            winner !== -1
-              ?
+            !border
+            &&
+            <h2 className="absolute top-20 sm:top-10 right-1/2 text-white text-4xl translate-x-1/2 sm:text-4xl w-full">Let's see Your Luck!</h2>
+          }
+          <div className="h-[8vh]">
+            {
+              border &&
               <>
+
                 {
-                  winner
+                  winner !== -1
                     ?
-                    <div className="text-2xl text-green-500"> You win </div>
+                    <>
+                      {
+                        winner
+                          ?
+                          <div className="text-4xl font-bold text-green-500 animate-opac"> You Win! </div>
+                          :
+                          <div className="text-4xl font-bold text-red-500 animate-opac"> Computer Win! </div>
+                      }
+                    </>
                     :
-                    <div className="text-2xl text-red-500"> Computer win! </div>
+                    <div className="text-4xl font-bold text-gray-400 animate-opac"> It's Tie </div>
                 }
               </>
-              :
-              <div className="text-2xl"> It's Tie </div>
-          }
-          <div className="flex gap-12 mt-8">
+            }
+          </div>
+          <div className="flex sm:gap-40 gap-10 mt-8">
             <div>
-              <div className="p-4 rounded-full border">
-                <img src={userChoice === 'rock' && rock || userChoice === 'paper' && paper || userChoice === 'scissors' && scissors} className="w-28 sm:w-36 animate-rot" alt="" />
+              <div
+                className={`p-5 rounded-full border-[5px] animate-color 
+                ${border ? winner === -1 ? ' border-gray-400' : winner === 1 ? ' border-green-600 border-[10px]' : 'border-red-600' : ''
+                  }
+                `}
+              >
+                <img src={userChoice === 'rock' && rock || userChoice === 'paper' && paper || userChoice === 'scissors' && scissors} className="w-28 sm:w-36 animate-hide" alt="" />
               </div>
-              <h2>You</h2>
+              <h2 className="text-4xl mt-6">You</h2>
             </div>
             <div>
-              <div className="p-4 rounded-full border">
-                <img src={computerChoice === 'rock' && rock || computerChoice === 'paper' && paper || computerChoice === 'scissors' && scissors} className="w-28 sm:w-36 animate-rot" alt="" />
+              <div
+                className={`p-5 rounded-full border-[5px] animate-colorreverse 
+                ${border ? winner === -1 ? ' border-gray-400' : winner === 1 ? 'border-red-600 border-[10px]' : 'border-green-600' : ''}
+                `}
+              >
+                <img src={computerChoice === 'rock' && rock || computerChoice === 'paper' && paper || computerChoice === 'scissors' && scissors} className="w-28 sm:w-36 animate-hide" alt="" />
               </div>
-              <h2>Com</h2>
+              <h2 className="text-4xl mt-6">Com</h2>
             </div>
           </div>
+          {
+            border
+            &&
+            <button className="text-2xl rounded-md bg-[#111153] hover:bg-[#0c0c36] border p-4 absolute bottom-20 right-1/2 translate-x-1/2 sm:text-2xl sm:bottom-6 sm:p-2 " onClick={() => handleClose()}>
+              Continue
+            </button>
+          }
         </div>
       }
     </>
--- React Assignment/rock-paper-scissors/tailwind.config.cjs
+++ React Assignment/rock-paper-scissors/tailwind.config.cjs
@@ -7,23 +7,56 @@ module.exports = {
   theme: {
     extend: {
       keyframes: {
-        rot: {
-          '0%': {
-            transform: 'scale(0.5)'
-          },
-          '100%': {
-             transform: 'scale(1)' 
-          }
-        },
         inc: {
           '0%': { transform: 'scale(.5)' },
           '50%': { transform: 'scale(0.75)' },
           '100%': { transform: 'scale(1)' }
-        }
+        },
+        opac : {
+          '0%': { opacity: '0' },
+          '50%': { opacity: '0'},
+          '75%': { opacity : '0'},
+          '100%': { opacity: '1' }
+        },
+        hide: {
+          '0%': { opacity: '0' },
+          '50%': { opacity : '0'},
+          '75%' : { opacity : '0'},
+          '100%': { opacity: '1' }
+        },
+        color : {
+          '0%' : { borderColor : "red" },
+          '10%': { borderColor: "green" },
+          '20%': { borderColor: "red" },
+          '30%': { borderColor: "green" },
+          '40%': { borderColor: "red" },
+          '50%': { borderColor: "green" },
+          '60%': { borderColor: "red" },
+          '70%': { borderColor: "green" },
+          '80%': { borderColor: "red" },
+          '90%': { borderColor: "green"},
+          '100%': { borderColor: "red"}, 
+        },
+        colorreverse: {
+          '0%': { borderColor: "green" },
+          '10%': { borderColor: "red" },
+          '20%': { borderColor: "green" },
+          '30%': { borderColor: "red" },
+          '40%': { borderColor: "green" },
+          '50%': { borderColor: "red" },
+          '60%': { borderColor: "green" },
+          '70%': { borderColor: "red" },
+          '80%': { borderColor: "green" },
+          '90%': { borderColor: "red" },
+          '100%': { borderColor: "green" },
+        },
       },
       animation: {
-        'rot': 'rot 1500ms linear 400ms 1',
-        'inc': 'inc 400ms linear 1'
+        'inc': 'inc 400ms linear 1',
+        'opac': 'opac 250ms linear 1',
+        'hide': 'hide 4500ms linear 1',
+        'color': 'color 4000ms linear 1',
+        'colorreverse': 'colorreverse 4000ms linear 1',
       },
 
     },
