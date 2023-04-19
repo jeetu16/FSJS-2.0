3c9143c3995d61cbaadbd65763652a8b9767b604
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-04-19 18:58:24 +0530

    navbar updated

--- React Assignment/movie-app/src/Components/Header.jsx
+++ React Assignment/movie-app/src/Components/Header.jsx
@@ -10,30 +10,38 @@ const Header = () => {
 
     const focusOnSearch = useRef(null);
     const [searchMovie, setSearchMovie] = useState("");
+    const [searchBtnToggle, setSearchBtnToggle] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         focusOnSearch.current.focus();
-        navigate(`/searchResult`, { state :searchMovie } )
+        navigate(`/searchResult`, { state: searchMovie })
         window.location.reload(true);
         setSearchMovie("");
+    }
+
+    const handleSearchBtn = (e) => {
+        setSearchBtnToggle(!searchBtnToggle);
+        alert("Hello");
     }
 
     return (
         <>
             <header className='bg-indigo-700 flex flex-row items-center p-2 text-white sm:flex-row sm:justify-between sticky top-0 z-10 px-6 justify-between'>
-                <GiHamburgerMenu className='cursor-pointer sm:hidden w-fit'/>
+                <button className='border px-2 py-1 sm:hidden' onClick={() => alert("hello")}>
+                    <GiHamburgerMenu className='cursor-pointer text-2xl' />
+                </button>
                 <div className='w-fittext-center py-2'>
                     <Link to="/" className='text-2xl font-bold font-mono sm:text-4xl tracking-wide'><span className="font-serif text-orange-500 ">M</span>ovie<span className='font-serif text-[papayawhip]'>B</span>uzz</Link>
                 </div>
-                <ul className='absolute hidden top-10 text-black sm:static gap-5 text-lg font-semibold sm:text-[papayawhip] sm:flex'>
+                <ul className='absolute top-20 hidden text-black sm:static gap-5 text-lg font-semibold sm:text-[papayawhip] sm:flex'>
                     <li className='cursor-pointer hover:opacity-80 duration-300 hover:scale-x-110'>Home</li>
                     <li className='cursor-pointer hover:opacity-80 duration-300 hover:scale-x-110'>Trending Movies</li>
                     <li className='cursor-pointer hover:opacity-80 duration-300 hover:scale-x-110'>Top Rated Movies</li>
                     <li className='cursor-pointer hover:opacity-80 duration-300 hover:scale-x-110'>Trending Shows</li>
-                    <li className='cursor-pointer hover:opacity-80 duration-300 hover:scale-x-110'>Top Rated Shows</li>           
+                    <li className='cursor-pointer hover:opacity-80 duration-300 hover:scale-x-110'>Top Rated Shows</li>
                 </ul>
-                <form className='sm:flex items-center justify-center mt-2 w-full px-2 text-xl text-black pb-1 sm:w-fit hidden' onSubmit={(e) => handleSubmit(e)}>
+                <form className='sm:flex items-center sm:justify-center mt-2 w-full px-2 text-xl text-black p-4 sm:p-2 sm:w-fit absolute sm:static top-[7vh] bg-indigo-700 border sm:border-none sm:bg-indigo-700 left-0 z-10 flex justify-around' onSubmit={(e) => handleSubmit(e)}>
                     <input
                         autoFocus
                         className='outline-none p-1 rounded'
@@ -46,13 +54,21 @@ const Header = () => {
                     />
                     <button
                         type='submit'
-                        className='text-white ml-2 p-0.5'
+                        className={`text-white ml-2 p-0.5 ${searchBtnToggle}`}
                         onClick={(e) => searchMovie && handleSubmit(e)}
                     >
-                        <FaSearch/>
+                        {
+                            searchBtnToggle
+                            ? 
+                            <FaSearch />
+                            :
+                            "Search"
+                        }
                     </button>
                 </form>
-                <FaSearch className='sm:hidden w-fit' />
+                <button className='sm:hidden border  py-1 px-2' onClick={handleSearchBtn}>
+                    <FaSearch className='text-2xl' />
+                </button>
             </header>
         </>
     )
